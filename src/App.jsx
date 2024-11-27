1b3fd2c9ff0bd09aca23709e423d4f11beac9318
omar tabbaa <omartabbaa0@gmail.com>
2024-11-27 15:58:34 +0100

    routing and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './page/LandingPage';
+import BusinessOverviewPage from './page/BusinessOverviewPage';
+import DepartmentProjectManagementPage from './page/Department&ProjectManagementPage';
+import QuestionOverviewPage from './page/QuestionOverviewPage';
+import QuestionDetailPage from './page/QuestionDetailPage';
+import AdmnDashboardPage from './page/AdmnDashboardPage';
+import NotFoundPage from './page/NotFoundPage';
+import Navbar from './Components/Sections/navbar/navbar';
+import Login from './page/Login';
+import SignUp from './page/SignUp';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <body>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/business-overview" element={<BusinessOverviewPage />} />
+        <Route path="/department-project-management/:businessId/:businessName" element={<DepartmentProjectManagementPage />} />
+        <Route path="/question-overview/:department/:project/:projectId" element={<QuestionOverviewPage />} />
+        <Route path="/question-detail/:questionId/:title/:question/:projectId" element={<QuestionDetailPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/admin-dashboard" element={<AdmnDashboardPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </body>
+
+
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './context/LoginContext.jsx';
+import { AnswerProvider } from './context/AnswerContext.jsx';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <AnswerProvider> 
+    <UserProvider> 
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserProvider>
+    </AnswerProvider>
+  </React.StrictMode>
+);
